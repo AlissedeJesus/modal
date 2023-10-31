5703edf387a60ed9741b541fa3d1c415b760f3a8
Alisse <alissecardoso5@gmail.com>
2023-10-31 16:51:00 -0300

    Subindo atualizaçoes do Modal

---
+++ js/style.js
@@ -0,0 +1,21 @@
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal")
+
+const Modal = document.querySelector("#modal")
+const fade = document.querySelector("#fade")
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+
+
+
+})
+    
+
+
+
+
+
+
+
+
+ 
