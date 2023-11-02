9c02d9459d5968c29b6fd434d6ee3ef1232a86f9
Alisse de Jesus <alissecardoso5@gmail.com>
2023-11-02 15:58:38 -0300

    atualizaçao

--- js/style.js
+++ js/style.js
@@ -1,19 +1,18 @@
 const openModalButton = document.querySelector("#open-modal");
-const closeModalButton = document.querySelector("#close-modal")
+const closeModalButton = document.querySelector("close-modal")
 
-const Modal = document.querySelector("#modal")
+const modal = document.querySelector("#modal")
 const fade = document.querySelector("#fade")
 
-const toggeleModal =() => {
+const toggeleModal = () => {
     Modal.classList.toggle("hide");
     fade.classList.toggle("hide");
 };   
 
 
-
+ 
 [openModalButton, closeModalButton, fade].forEach((el) => {
-    el.addeventlistener("click",() => console.log("teste"));
-
+    el.addEventListener("click", () => toggeleModal());
 
 
 })
