69117e4914382d7f54bb1fa65e07b2d379ec1295
Alisse de Jesus <alissecardoso5@gmail.com>
2023-11-02 14:20:24 -0300

    modal-atualizações

--- js/style.js
+++ js/style.js
@@ -4,7 +4,15 @@ const closeModalButton = document.querySelector("#close-modal")
 const Modal = document.querySelector("#modal")
 const fade = document.querySelector("#fade")
 
+const toggeleModal =() => {
+    Modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+};   
+
+
+
 [openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addeventlistener("click",() => console.log("teste"));
 
 
 
