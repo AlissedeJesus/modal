2dbab42b8460a4e6917e62c943224887df23e6f2
Alisse <alissecardoso5@gmail.com>
2023-11-08 16:46:58 -0300

    subindo

--- js/style.js
+++ js/style.js
@@ -1,21 +1,28 @@
 const openModalButton = document.querySelector("#open-modal");
-const closeModalButton = document.querySelector("close-modal")
+const closemodalButton = document.querySelector("close-modal")
 
 const modal = document.querySelector("#modal")
 const fade = document.querySelector("#fade")
 
 const toggeleModal = () => {
-    Modal.classList.toggle("hide");
+    modal.classList.toggle("hide");
     fade.classList.toggle("hide");
 };   
 
 
  
-[openModalButton, closeModalButton, fade].forEach((el) => {
+[openModalButton, closemodalButton, fade].forEach((el) => {
     el.addEventListener("click", () => toggeleModal());
 
 
 })
+
+
+function closeModal(){
+    document.getElementById("close-modal").style.display = "none"
+}
+
+
     
 
 
